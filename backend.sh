4d39686538f92dedd5641cdff325973bec98a4db
Thoufiq <tshaik202@gmail.com>
2026-04-02 10:19:19 +0530

    expense-shell added

---
+++ backend.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%m-%s)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+if [ $1 -ne 0 ] 
+then
+    echo -e "$2 ... $R Failure $N"
+    exit 1
+else
+    echo -e "$2 ... $G Success $N"
+     fi    
+}
+
+CHECK_ROOT () {
+    if [ $USERID -ne 0 ]
+    then 
+    echo "ERROR:: you must have sudo access to execute this script"
+    exit 1 
+fi
+}   
+
+echo "Script started executing at: $TIMESTAMP" &>>LOG_FILE_NAME
+
+CHECK_ROOT 
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Mysql Server"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Mysql Server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Mysql Server"
+
+mysql -h mysql.daws82s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql Root password not setup" &>>$LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+else
+    echo -e "Mysql Root password already setup ... $Y SKIPPING $N"
+fi
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? " Disabling existing default NodeJS"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? " Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? " Installing NodeJS 20"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE $? "Adding Expense user"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading backend"
+
+cd /app
+
+unzip /tmp/backend.zip&>>$LOG_FILE_NAME
+VALIDATE $? "unzip backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# prepare Mysql schema ::
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing mysql client"
+
+mysql -h mysql.daws82s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+VALIDATE $? "Setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "daemom reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
+
+systemctl start backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Backend"
